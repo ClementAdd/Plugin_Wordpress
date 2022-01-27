382731633fb0f39d590f51419e605289daef068a
Clément Addario <71002854+ClementAdd@users.noreply.github.com>
2022-01-27 10:08:55 +0100

    Changement de channel telegram OK !

--- plugin-telegram.php
+++ plugin-telegram.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Ygram
 Plugin URI: https://git.ytrack.learn.ynov.com/CADDARIO/project_challenge_php
-Description: Plugin for Ynov projet.
+Description: Vous devez créer un plugin Discord ou Télégram pour WordPress. Le plugin doit être capable (au minimum) de récupérer les commentaires d’un post pour les publier sur un canal Discord / Télégram précis. L’objectif est de recevoir une notification Discord / Télégram pour chaque commentaire posté.
 Author: Clément Addario
 Version: 1.0.1
 Author URI: https://github.com/ClementAdd
@@ -16,20 +16,33 @@ function my_admin_menu()
         __('Telegram notification', 'telegram-notification'),
         'manage_options',
         'telegram',
-        'telegram_notif',
-        'https://cdn-icons.flaticon.com/png/128/3670/premium/3670044.png?token=exp=1642770390~hmac=84d28deab6081bdcc22bc15bd82c3eae',
+        'display_admin_page',
+        'dashicons-format-chat',
         20
     );
 }
 
 add_action('admin_menu', 'my_admin_menu');
 
+
+function init_plugin()
+{
+    add_option('chatID');
+
+}
+
+register_activation_hook(__FILE__, 'init_plugin');
+
+
 function show_message_function($comment_ID, $comment_approved)
 {
+
+
+
     if (1 === $comment_approved) {
         $comment = get_comment($comment_ID);
         $author = $comment->comment_author;
-        $subject = 'Nouveau commentaire de :' . ucfirst($author);
+        $subject = 'Nouveau commentaire de : ' . ucfirst($author);
         $message = $comment->comment_content;
         telegram_notif($subject . "\n" . $message);
     }
@@ -38,22 +51,54 @@ function show_message_function($comment_ID, $comment_approved)
 add_action('comment_post', 'show_message_function', 10, 2);
 
 
-function telegram_notif($comment)
+function telegram_notif($comment = "")
 {
+
+
+    $chatID = get_option('chatID');
     $apiToken = "5057919374:AAHOaQoblgzAo2xANn7yg95OI901XYr6M8U";
     $data = [
-        'chat_id' => '-1001739056597',
+        'chat_id' => $chatID,
         'text' => $comment
     ];
-    $response = file_get_contents("https://api.telegram.org/bot$apiToken/sendMessage?" . http_build_query($data));
+    if ($comment != '') {
+        $response = file_get_contents("https://api.telegram.org/bot$apiToken/sendMessage?" . http_build_query($data));
+        if ($response == null) {
+            exit();
+        }
+    }
 
+}
 
+function display_admin_page()
+{
     ?>
     <h1>
         <?php esc_html_e('Notification Telegram', 'notif-telegram'); ?>
     </h1>
+
+    <form action="admin.php?page=telegram" method="post">
+
+
+        <input type="text" name="chatID" placeholder="ChatId telegram">
+
+        <input type="submit" name="submit">
+
+        <?php
+
+        $id = "";
+        if ( isset( $_POST['submit'] ) ) {
+             $id = $_POST['chatID'];
+            echo 'ChatID Actuel : '.$id;
+        }
+
+        $options = update_option('chatID', $id);
+        ?>
+
+    </form>
+
     <?php
+
+
 }
-
-
 
